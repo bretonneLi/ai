849fdf24ea0be6774eff224da1616144e0ef138e
BenhaoDU <dubenhao@gmail.com>
2023-11-27 17:26:08 +0100

    chatbot

--- chatbot.py
+++ chatbot.py
@@ -3,48 +3,34 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains import LLMChain
 from langchain.llms import LlamaCpp
 from langchain.prompts import PromptTemplate
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationSummaryBufferMemory
-from langchain.prompts import (
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-    MessagesPlaceholder,
-    SystemMessagePromptTemplate,
-)
 from langchain.memory import ConversationBufferMemory
-from langchain.memory import ConversationSummaryMemory
-
+# Callbacks support token-wise streaming
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 llm = LlamaCpp(
     model_path="/home/dubenhao/llama/llama-2-7b-chat/ggml-model-q4_k_m.gguf",
-    temperature=0.75,
-    max_tokens=2000,
-    top_p=1,
-    callback_manager=callback_manager,
-    verbose=True,  # Verbose is required to pass to the callback manager
+    n_gpu_layers=1,
+    n_batch=512,
+    n_ctx=2048,
+    f16_kv=True,
+    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+    verbose=True,# Verbose is required to pass to the callback manager
 )
-
 # Prompt
-prompt = ChatPromptTemplate(
-    messages=[
-        SystemMessagePromptTemplate.from_template(
-            "You are a nice chatbot having a conversation with a human."
-        ),
-        # The `variable_name` here is what must align with memory
-        MessagesPlaceholder(variable_name="chat_history"),
-        HumanMessagePromptTemplate.from_template("{question}"),
-    ]
+prompt = PromptTemplate(
+    input_variables=["question","chat_history"],
+    template="""<s>[INST] <<SYS>>
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+<</SYS>>
+
+{chat_history}
+{question} [/INST]
+""",
 )
-
-# Notice that we `return_messages=True` to fit into the MessagesPlaceholder
-# Notice that `"chat_history"` aligns with the MessagesPlaceholder name
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-conversation = LLMChain(llm=llm, prompt=prompt, verbose=True, memory=memory)
-
-# Notice that we just pass in the `question` variables - `chat_history` gets populated by memory
-
-conversation({"question": "hi"})
-
+#memory
+memory = ConversationBufferMemory(memory_key="chat_history")
+conversation = LLMChain(llm=llm, prompt=prompt, memory=memory)
 conversation(
     {"question": "Translate this sentence from English to French: I love programming."}
 )
