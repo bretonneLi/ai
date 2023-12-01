a5b62f25fce12a0f49dab1de5c55010f47074423
BenhaoDU <dubenhao@gmail.com>
2023-12-01 09:36:36 +0100

    chatbot retrieval

--- chatbot_retriever.py
+++ chatbot_retriever.py
@@ -17,46 +17,43 @@ llm = LlamaCpp(
     n_gpu_layers=1,
     n_batch=512,
     n_ctx=4096,
-    f16_kv=True,
-    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
-    verbose=True,# Verbose is required to pass to the callback manager
+#    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+#    verbose=True,# Verbose is required to pass to the callback manager
 )
+# 
 prompt = PromptTemplate(
     input_variables=["question","chat_history"],
-    template="""<s>[INST] <<SYS>>
-You are a helpful, respectful and honest assistant.
-<</SYS>>
-Here is the context:{context}
-Question:{question} [/INST]
+    template="""[INST]<<SYS>> You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question.
+If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.<</SYS>> 
+Chat_history:{chat_history}
+Question: {question} 
+ 
+Answer: [/INST]
 """,
 )
-loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-    bs_kwargs=dict(
-        parse_only=bs4.SoupStrainer(
-            class_=("post-content", "post-title", "post-header")
-        )
-    ),
-)
+loader = loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
 docs = loader.load()
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 splits = text_splitter.split_documents(docs)
 
 from langchain.embeddings import CacheBackedEmbeddings
 from langchain.storage import LocalFileStore
-underlying_embeddings = LlamaCppEmbeddings(model_path="/home/dubenhao/llama/llama-2-7b/ggml-model-q4_k_m.gguf")
-fs = LocalFileStore("/home/dubenhao/cache/")
-cached_embedder = CacheBackedEmbeddings.from_bytes_store(
-    underlying_embeddings, fs,
-)
+underlying_embeddings = LlamaCppEmbeddings(model_path="/home/dubenhao/llama/llama-2-7b-chat/ggml-model-q4_k_m.gguf")
+fs = LocalFileStore("/home/dubenhao/cache_cr/")
+cached_embedder = CacheBackedEmbeddings.from_bytes_store(underlying_embeddings, fs)
 
 vectorstore = Chroma.from_documents(splits,cached_embedder)
 retriever = vectorstore.as_retriever()
-memory = ConversationBufferMemory(
-   memory_key="chat_history", return_messages=True
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True
 )
 
-qa = ConversationalRetrievalChain.from_llm(llm, retriever=retriever,verbose=False, memory=memory)
-qa("How do agents use Task decomposition?")
+qa = ConversationalRetrievalChain.from_llm(llm, retriever=retriever,condense_question_prompt=prompt, memory=memory)
 
-qa("What are the various ways to implement memory to support it?")
+questions=[
+    "How do agents use Task decomposition?",
+    "What are the various ways to implement memory to support it?"
+]
+for question in questions:
+    result = qa(question)
+    print(f"-> **Question**: {question} \n")
+    print(f"**Answer**: {result['answer']} \n")
