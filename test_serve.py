5e162f84cfa359f4f1236df297d5cbff06612018
BenhaoDU <dubenhao@gmail.com>
2024-01-19 15:11:12 +0100

    test_serve

--- test_serve.py
+++ test_serve.py
@@ -9,6 +9,11 @@ from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import LLMChain
 from langchain.llms import LlamaCpp
 from langchain.prompts import PromptTemplate
+from langchain.embeddings import LlamaCppEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 llm = LlamaCpp(
     model_path="/home/dubenhao/llama/llama-2-7b-chat/ggml-model-q4_k_m.gguf",
@@ -31,8 +36,6 @@ add_routes(
     path="/llamacpp",
 )
 
-
-
 model = llm
 prompt_chatbot = PromptTemplate(
     input_variables=["question","chat_history"],
@@ -50,9 +53,50 @@ add_routes(
     conversation,
     path="/chatbot",
 )
+from langchain_community.document_loaders import WebBaseLoader
+loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+data = loader.load()
+
+# Split
+from langchain_community.vectorstores import Chroma
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+all_splits = text_splitter.split_documents(data)
+
+# Add to vectorDB
+vectorstore = Chroma.from_documents(documents=all_splits, 
+                                    collection_name="rag-chroma",
+                                    embedding=LlamaCppEmbeddings(model_path="/home/dubenhao/llama/llama-2-7b-chat/ggml-model-q4_k_m.gguf"),
+                                    )
+retriever = vectorstore.as_retriever()
+template="""[INST]
+    Use the following pieces ofcontext to answer the question.If no context provided, answer like a AI assistant. 
+    
+    {context} 
+    Question: {question}
+    [/INST]
+"""
 
 
-
+'''#
+vectorstore=FAISS.load_local("/home/dubenhao/vectorstore/db_faiss", embedding=LlamaCppEmbeddings(model_path="/home/dubenhao/llama/llama-2-7b-chat/ggml-model-q4_k_m.gguf"))
+retriever = vectorstore.as_retriever(
+        search_kwargs={"k": 6}
+    )
+'''
+qa_chain = RetrievalQA.from_chain_type(
+    llm=llm, 
+    retriever=retriever,     
+    chain_type_kwargs={
+        "prompt":  PromptTemplate(
+            template=template,
+            input_variables=["context", "question"]),
+    }
+)
+add_routes(
+    app,
+    qa_chain,
+    path="/qa_chain",
+)
 if __name__ == "__main__":
     import uvicorn
 
